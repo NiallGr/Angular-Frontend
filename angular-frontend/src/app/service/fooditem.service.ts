83dc4a89ee9e7c0feb0a033551f36c6089bba6b9
Niall Grainger <niallgrainger@gmail.com>
2021-06-10 22:12:44 -0700

    Comments

--- angular-frontend/src/app/service/fooditem.service.ts
+++ angular-frontend/src/app/service/fooditem.service.ts
@@ -12,23 +12,23 @@ export class FooditemService {
     private baseURL = "http://localhost:8081/api/v1/foodItems";
  
   constructor(private httpClient: HttpClient) { }
-
+    // retrieve items from Backend
   getfooditemsList(): Observable<FoodItem[]> {
  return this.httpClient.get<FoodItem[]>(`${this.baseURL}`);
   }
-
+    // Create item 
   createFooditem(fooditem: FoodItem): Observable<Object> {
     return this.httpClient.post(`${this.baseURL}`, fooditem);
   }
-  // Get food Item by ID from the Spring/Database
+  // Get food Item by ID from the backend
   getfoodItemById(id: number): Observable<FoodItem>{
     return this.httpClient.get<FoodItem>(`${this.baseURL}/${id}`);
   }
-
+    // updated Item by ID and Inputs
   updateFoodItem(id: number, fooditem: FoodItem): Observable<object> {
     return this.httpClient.put(`${this.baseURL}/${id}`, fooditem);
   }
-
+  // delete item 
   deleteFoodItem(id: number): Observable<object>{
     return this.httpClient.delete(`${this.baseURL}/${id}`);
   }
